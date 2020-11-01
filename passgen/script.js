5ecc8f276a90d4c8a597cd25010d3d8a9e1dddd5
Melanie <mschellberg27@icloud.com>
2020-11-01 17:10:50 -0500

    generated password complete

--- passgen/script.js
+++ passgen/script.js
@@ -1,52 +1,64 @@
-function randomSpecialChar () {
-  // example set of special chars as a string in no particular order
-  var s = "!$%&?*@#";
-
-  // generating a random index into the string and extracting the character at that position
-  return s.substr(Math.floor(s.length*Math.random()), 1);
-} 
-
-
 // Assignment code here
+function createRandomCharacter(randomStr, begin, end) {
+  var randomValue = begin + Math.floor(Math.random() * (end - begin))
+  return (randomStr + String.fromCharCode(randomValue))
+}
+
+function createRandomSpecialCharacter(randomStr) {
+  var specialCharacters = ["?", "!", "@", "%", "#"]
+  var index = Math.floor(Math.random() * specialCharacters.length)
+  return randomStr + specialCharacters[index]
+}
+
+function getRandomString(length, useUpper, useLower, useNumber, useSpecialCharacter) {
+  var randomStr = ""
+  var begin = 0
+  var end = 0
+  for (ii = 0; ii < length; ii++) {
+    if (useUpper) {
+      var begin = 65
+      var end = 91
+      randomStr = createRandomCharacter(randomStr, begin, end);
+    }
+    if (useLower) {
+      var begin = 97
+      var end = 123
+      randomStr = createRandomCharacter(randomStr, begin, end);
+    }
+    if (useNumber) {
+      var begin = 48
+      var end = 58
+      randomStr = createRandomCharacter(randomStr, begin, end);
+    }
+    if (useSpecialCharacter) {
+      randomStr = createRandomSpecialCharacter(randomStr)
+
+    }
+  }
+  return randomStr.substring(0, length)
+}
+
 function generatePassword() {
   var password = "";
-  var passwordLengthString = window.prompt("How many character would you like your password to contain?");
+  var passwordLengthString = window.prompt("How many characters would you like your password to contain?");
   var passwordLength = parseInt (passwordLengthString);
 // Validating password length 
-  if (passwordLength < 8 || passwordLength > 20) {
-    alert ("Password must be between 8 and 20 characters");
-    return "no password generated";
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert ("Password must be between 8 and 128 characters");
+    return "No password generated";
   }
 // Special Characters
   var specialCharacters = window.confirm("Click OK to confirm including special characters.");
-    if (specialCharacters) {
-    password = password + randomSpecialChar();
-  }
-
 // Numeric Numbers
   var numbers = window.confirm("Click OK to confirm including numeric characters");
-    if (numbers) {
-    password = password + Math.floor(Math.random() * 10);
-}
-
 // Uppercase
-  var upperCase = window.confirm("Click OK to confrim including uppercase characters.");
-    if (upperCase) {
-    password = password + "M";
-  }
-
+  var upperCase = window.confirm("Click OK to confirm including uppercase characters.");  
 // Lowercase 
   var lowerCase = window.confirm("Click OK to confrim including lowercase characters.");
-    if (lowerCase) {
-    password = password + "m";
-  }
-
+    
+password = getRandomString(passwordLength, upperCase, lowerCase, numbers, specialCharacters); //Needs to be the same order as the function above
 return password;
 }
-
-
-
-
 
 
 
